98bbce27204986d44b3a291b62ce4579fed05e0f
Kevin Daniel Solano Cardenas <102763397+Kevinsolanoxd@users.noreply.github.com>
2022-04-21 17:34:03 -0500

    Add files via upload

---
+++ Escritorio/10/potencia/README.md
@@ -0,0 +1,3 @@
+# programa NO.3: comvertir grados sentigrados a k y f .
+
+¡[diagrama de flujo ](diagrama.png "diagrama deflujo")
---
+++ Escritorio/10/potencia/temperatura.py
@@ -0,0 +1,9 @@
+"--este programa es para  calcular los gardos en f y kelvin--"
+#input
+c=int(input("digite los grados"))
+#processing
+
+k=c+273.15
+f=c*1.8+32
+#ouput
+print(str(c) + " grados celsius" + " en kelvin es " + str(k) + " y en fahrenheit es " + str(f))

aadee79481e443945b36ce5bd935e7b349c83740
Kevinsolanoxd <kevinsolano@colegioguanenta.edu.co>
2022-04-21 16:28:00 -0500

    Desarrollo: creacion de diagrama de flujo

---
+++ Escritorio/10/potencia/potencia.py
@@ -0,0 +1,11 @@
+"""programa No. 2: programa para calcular x a la y"""
+print("---------------------")
+print("--potencia x a la y--")
+print("---------------------")
+# input
+x=int(input("dijite x:"))
+y=int(input("dijite y:"))
+# procesing
+z=x**y
+#ouput
+print(str(x) + " a la " +str(y) + " es " + str(z))
